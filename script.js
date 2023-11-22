fd89ccfc0b8fae4d7805e76f8f5c241867373a73
Amirhossein <4mirh03einhashemi@gmail.com>
2023-11-22 19:11:34 +0330

    create app class and its functionality base on architecture part one

--- script.js
+++ script.js
@@ -10,3 +10,78 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+class App {
+  #map;
+  #mapEvent;
+
+  constructor() {
+    this._getPosition();
+    inputType.addEventListener('change', this._toggleElevationField);
+    form.addEventListener('submit', this._newWorkout.bind(this));
+  }
+
+  _getPosition() {
+    navigator.geolocation.getCurrentPosition(
+      this._loadMap.bind(this),
+      function () {
+        alert('Can not get your current position');
+      }
+    );
+  }
+
+  _loadMap(position) {
+    // receive user location coords
+    const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude];
+
+    // display map
+    this.#map = L.map('map').setView(coords, 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // add event listener to map
+    this.#map.on('click', this._showForm.bind(this));
+  }
+
+  _showForm(mapE) {
+    // receive and store data about coords that user clicked
+    this.#mapEvent = mapE;
+
+    form.classList.remove('hidden');
+    inputDistance.focus();
+  }
+
+  _toggleElevationField() {
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  _newWorkout(e) {
+    e.preventDefault();
+
+    // clear inputs
+    inputCadence.value =
+      inputDistance.value =
+      inputDuration.value =
+      inputElevation.value =
+        '';
+
+    // display marker on the coords that user clicked
+    const { lat, lng } = this.#mapEvent.latlng;
+    const popup = L.popup({
+      maxWidth: 250,
+      minWidth: 100,
+      autoClose: false,
+      closeOnClick: false,
+      content: 'hi',
+      className: 'cycling-popup',
+    });
+
+    L.marker([lat, lng]).addTo(this.#map).bindPopup(popup).openPopup();
+  }
+}
+
+const app = new App();
